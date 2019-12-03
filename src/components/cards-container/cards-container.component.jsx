8a33daf7d533ddd3db047a495256fd96a360f958
Charles Clark <ctcb57@gmail.com>
2019-12-02 21:55:31 -0600

    Created header and card-container components along with some basic styling to start

---
+++ src/components/cards-container/cards-container.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './cards-container.styles.css';
+
+function CardsContainer(){
+    return(
+        <div className="ui rasied very padded text container segment">
+            <p>Content displays here</p>
+        </div>
+    );
+}
+
+export default CardsContainer;
---
+++ src/components/page-header/page-header.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './page-header.styles.css';
+
+function PageHeader(){
+    return(
+        <div className="header">Flash Cards</div>
+    );
+}
+
+export default PageHeader;
